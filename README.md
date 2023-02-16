f9c9e6da5aa8a4f2beb654259fa42b21ea53c51a
john <john.mosquera@vodafone.com>
2023-02-16 16:12:41 +0000

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+Following https://www.youtube.com/watch?v=AKQWiqMM4Nk to create front end - python Flask for backend semantic-search
+using openai - chatgpt
---
+++ delete.py
@@ -0,0 +1,11 @@
+{"text": " https://www.ema.europa.eu/en/human-regulatory/research-development/paediatric-medicines/paediatric-investigation-plans/paediatric-investigation-plans-questions-answers This page provides detailed guidance for companies intending to ", 
+  "segments": [
+    {"start": 1,
+     "end": 2,
+     "text": "https://www.ema.europa.eu/en/human-regulatory/research-development/paediatric-medicines/paediatric-investigation-plans/paediatric-investigation-plans-questions-answers"},
+    {"start": 13,
+     "end": 14,
+     "text": "The assignment of an RPI has no regulatory consequences on the status or classification of the product"},
+    {"start": 603,
+     "end": 604,
+     "text": "A discussion meeting with PDCO will be arranged at the applicant's request, preferably at day 90 in case of an initial PIP procedure, or at day 30 in case of a modification of an agreed PIP procedure, where the network representative(s) take part together with the applicant."}]}
